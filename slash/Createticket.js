9e0eccc5765bb2517d710cedb9eea0b93fbade7e
Creatorsbbs <serisvaldo927@gmail.com>
2026-06-10 14:59:32 -0300

    Remove unused imports in Createticket.js

--- slash/Createticket.js
+++ slash/Createticket.js
@@ -1,5 +1,3 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ticket")
